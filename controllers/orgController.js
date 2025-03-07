1b7dc0646a5050758e4ed3b3d388e1cda95ad027
AbeDevSaga <2000amandanm@gmail.com>
2025-03-08 00:03:51 +0300

    update_org

--- controllers/orgController.js
+++ controllers/orgController.js
@@ -25,7 +25,7 @@ const createUser = async (req, res) => {
   }
 };
 
-const createOrg = async (req, res) => {
+const createOrganization = async (req, res) => {
   try {
     const { name } = req.body;
 
@@ -50,31 +50,55 @@ const createOrg = async (req, res) => {
   }
 };
 
-const getAllOrganizations = async (req, res) => {
+const updateOrganization = async (req, res) => {
     try {
-        const services = await Service.find();
-        res.status(200).json(services);
+        const { id } = req.params;
+        const updates = req.body;
+        const updatedOrganization = await Organization.findByIdAndUpdate(id, updates, { new: true });
+        if (!updatedOrganization) return res.status(404).json({ message: 'Organization not found' });
+        res.status(200).json({ message: 'Organization updated successfully', organization: updatedOrganization });
     } catch (error) {
-        res.status(500).json({ message: 'Failed to get services', error });
+        res.status(500).json({ message: 'Failed to update Organization', error });
     }
 };
 
-// Get a single service by ID
+const deleteOrganization = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedOrganization = await Organization.findByIdAndDelete(id);
+        if (!deletedOrganization) return res.status(404).json({ message: 'Organization not found' });
+        res.status(200).json({ message: 'Organization deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to delete Organization', error });
+    }
+};
+
+const getAllOrganizations = async (req, res) => {
+    try {
+        const organization = await Organization.find();
+        res.status(200).json(organization);
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to get organization', error });
+    }
+};
+
 const getOrganizationById = async (req, res) => {
     try {
         const { id } = req.params;
-        const service = await Service.findById(id);
-        if (!service) return res.status(404).json({ message: 'Service not found' });
-        res.status(200).json(service);
+        const organization = await Organization.findById(id);
+        if (!organization) return res.status(404).json({ message: 'Organization not found' });
+        res.status(200).json(organization);
     } catch (error) {
-        res.status(500).json({ message: 'Failed to get service', error });
+        res.status(500).json({ message: 'Failed to get Organization', error });
     }
 };
 
 
 module.exports = {
     createUser,
-    createOrg,
+    createOrganization,
+    updateOrganization,
+    deleteOrganization,
     getAllOrganizations,
     getOrganizationById
   };
--- routes/orgRoutes.js
+++ routes/orgRoutes.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, createOrg, getAllOrganizations, getOrganizationById } = require('../controllers/orgController');
+const { createUser, createOrganization, getAllOrganizations, getOrganizationById, updateOrganization, deleteOrganization } = require('../controllers/orgController');
 const { verifyToken, isAdmin, isSuperAdmin } = require('../middlewares/authMiddleware');
 
 
-// Organization Basic Routes
-router.post('/create_org', verifyToken, isAdmin, createOrg);
+// Admin Basic Routes
+router.post('/create_org', verifyToken, isAdmin, createOrganization);
+router.put("/update_org/:id", verifyToken, isAdmin, updateOrganization);
+router.delete("/delete_org/:id", verifyToken, isAdmin, deleteOrganization);
+
+// SuperAdmin Basic Routes
 router.post('/create_user', verifyToken, isSuperAdmin, createUser);
-router.get("/", getAllOrganizations);
-router.get("/:id", getOrganizationById);
+
+router.get("/",verifyToken, isAdmin, getAllOrganizations);
+router.get("/:id",verifyToken, getOrganizationById);
 
 
 
