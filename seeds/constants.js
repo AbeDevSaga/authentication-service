67d5e5eb0e53e958e337c3ce3986443d61690552
AbeDevSaga <2000amandanm@gmail.com>
2025-03-08 02:29:11 +0300

    logo fo all the input files changed to the new path

--- seeds/constants.js
+++ seeds/constants.js
@@ -1,7 +1,9 @@
 const getRandomDate = () => {
   const start = new Date(2022, 0, 1); // Start date (January 1, 2022)
   const end = new Date(); // End date (current date)
-  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  return new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  );
 };
 
 const adminUser = {
@@ -25,7 +27,7 @@ const organizations = [
     phone: "+1 123 456 7890",
     email: "info@techinnovators.com",
     website: "https://techinnovators.com",
-    logo: "https://via.placeholder.com/150",
+    logo: "https://img.freepik.com/free-vector/bird-colorful-gradient-design-vector_343694-2506.jpg?t=st=1741389019~exp=1741392619~hmac=62cb12ed7cd6ff048cfbf2dc538117901cb318b9ae412373ae220c23c700b3bb&w=1060",
     created_at: getRandomDate(),
   },
   {
@@ -35,7 +37,7 @@ const organizations = [
     phone: "+1 234 567 8901",
     email: "info@greensolutions.com",
     website: "https://greensolutions.com",
-    logo: "https://via.placeholder.com/150",
+    logo: "https://img.freepik.com/free-psd/gradient-versus-logo-template_23-2151514112.jpg?t=st=1741389265~exp=1741392865~hmac=7312b8defcf4c92f3297cf7de210da5e0741a498f8eb0879db76eea40eb97f14&w=1060",
     created_at: getRandomDate(),
   },
   {
@@ -45,7 +47,7 @@ const organizations = [
     phone: "+1 345 678 9012",
     email: "info@healthplus.com",
     website: "https://healthplus.com",
-    logo: "https://via.placeholder.com/150",
+    logo: "https://img.freepik.com/free-vector/hospital-logo-design-vector-medical-cross_53876-136743.jpg?t=st=1741389414~exp=1741393014~hmac=af8027c90708ba330bcdb78a8bc8255a865e4641b7b639a2bf83d91f4ccaef4a&w=1060",
     created_at: getRandomDate(),
   },
   {
@@ -55,7 +57,7 @@ const organizations = [
     phone: "+1 456 789 0123",
     email: "info@edutech.com",
     website: "https://edutech.com",
-    logo: "https://via.placeholder.com/150",
+    logo: "https://img.freepik.com/free-vector/international-student-day-banner-design_1308-122226.jpg?t=st=1741389470~exp=1741393070~hmac=5a732fe57989c2ef0416c4755db90640f7d4ea1f1d9000fffa5d8e8e88b2240b&w=1060",
     created_at: getRandomDate(),
   },
   {
@@ -65,7 +67,7 @@ const organizations = [
     phone: "+1 567 890 1234",
     email: "info@foodiesdelight.com",
     website: "https://foodiesdelight.com",
-    logo: "https://via.placeholder.com/150",
+    logo: "https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148987940.jpg?t=st=1741389724~exp=1741393324~hmac=82f9d2ef75339fae61b166a70ea2a5704c888716e926770792568ae64d805525&w=1060",
     created_at: getRandomDate(),
   },
   {
@@ -75,7 +77,7 @@ const organizations = [
     phone: "+1 678 901 2345",
     email: "info@fashionforward.com",
     website: "https://fashionforward.com",
-    logo: "https://via.placeholder.com/150",
+    logo: "https://img.freepik.com/free-vector/hand-drawn-clothing-store-logo-design_23-2149577874.jpg?t=st=1741389820~exp=1741393420~hmac=57c07e109e694505c6d4715ca7b5efc5f78a5c217bb95dabd574dd924a0e2d12&w=1800",
     created_at: getRandomDate(),
   },
   {
@@ -85,7 +87,7 @@ const organizations = [
     phone: "+1 789 012 3456",
     email: "info@travelmasters.com",
     website: "https://travelmasters.com",
-    logo: "https://via.placeholder.com/150",
+    logo: "https://img.freepik.com/free-vector/detailed-travel-logo_23-2148627268.jpg?t=st=1741389914~exp=1741393514~hmac=810cdc2f4358e69a029b9fe6523e56050f66c0b28b8f78bac92e6e033a243db3&w=1060",
     created_at: getRandomDate(),
   },
   {
@@ -95,7 +97,7 @@ const organizations = [
     phone: "+1 890 123 4567",
     email: "info@fitnesszone.com",
     website: "https://fitnesszone.com",
-    logo: "https://via.placeholder.com/150",
+    logo: "https://img.freepik.com/free-vector/fitness-logo-template-flat-style_23-2148220483.jpg?t=st=1741389976~exp=1741393576~hmac=92f4328330b9fab249fbfa19b24ecf1b52d477ec078a5830b596430c449e45c8&w=1060",
     created_at: getRandomDate(),
   },
   {
@@ -105,7 +107,7 @@ const organizations = [
     phone: "+1 901 234 5678",
     email: "info@artistryhub.com",
     website: "https://artistryhub.com",
-    logo: "https://via.placeholder.com/150",
+    logo: "https://img.freepik.com/free-vector/colorful-letter-gradient-logo-design_474888-2309.jpg?t=st=1741390042~exp=1741393642~hmac=d28484509b71e7c7b8cbeb1095cc86fec5fd907813b384dab204fd3f03d9828d&w=1060",
     created_at: getRandomDate(),
   },
   {
@@ -115,7 +117,7 @@ const organizations = [
     phone: "+1 012 345 6789",
     email: "info@financepro.com",
     website: "https://financepro.com",
-    logo: "https://via.placeholder.com/150",
+    logo: "https://img.freepik.com/free-vector/gradient-accounting-logo-template_23-2148867917.jpg?t=st=1741390095~exp=1741393695~hmac=4b399b96a1cc7249843b9299fae1c9df914bff374c25d317c70f9a086ad14a82&w=1060",
     created_at: getRandomDate(),
   },
 ];
