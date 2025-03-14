ff49729834261934385b0c906a1cf7ecfde0d771
AbeDevSaga <2000amandanm@gmail.com>
2025-03-15 01:11:32 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,19 +12,19 @@ const login = async (req, res) => {
     if (!isPasswordValid)
       return res.status(401).json({ error: "Invalid password" });
 
-    const user_data = {
-      _id: user._id,
-      username: user.username,
-      email: user.email,
-      role: user.role,
-    };
+    // const user_data = {
+    //   _id: user._id,
+    //   username: user.username,
+    //   email: user.email,
+    //   role: user.role,
+    // };
 
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
       { expiresIn: "1d" }
     );
-    res.status(200).json({ token, user: user_data });
+    res.status(200).json({ token, user });
   } catch (error) {
     res.status(500).json({ error: "Login failed", details: error });
   }
