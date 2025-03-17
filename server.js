f1afe157cd3f01600eec26e7ab45780aa6a51141
AbeDevSaga <2000amandanm@gmail.com>
2025-03-17 15:21:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-// app.use(cors());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://yourfrontenddomain.com"], // Allow frontend
-    credentials: true, // Allow credentials (cookies, authorization headers)
+    origin: ["http://localhost:3000", "https://yourfrontenddomain.com"], 
+    credentials: true,
   })
 );
 connectDB();
